effd9fe7f99239656821cb4e2496f2300337e2f2
mibreit-photo <Michael.Breitung@mibreit-photo.com>
2021-04-05 14:20:26 +0200

    medium: fixed slideshow template - added example

---
+++ src/pages/slideshow.php
@@ -0,0 +1,12 @@
+{% from "./macros/mibreit-gallery.njk" import slideshow, prepare, scriptSlideshow %}
+{% set page_title="Homepage Template Title" %}
+{% set page_sub_title="Homepage Template Subtitle" %}
+{% set page_keywords="Homepage, template, keywords" %}
+{% set page_description="Homepage Template Description." %}
+{% set page_canonical = "" %}
+{% set page_active_navigation = "Slideshow" %}
+{% set page_de = "/german/" %}
+{% extends "./layouts/en/slideshow-content-layout.njk" %}
+{% block content %}
+  
+{% endblock %}
