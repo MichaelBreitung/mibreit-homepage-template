c53988f5bda0fd66b9662ee4be99c840c257eefc
MichaelBreitung <mibreit@gmx.de>
2022-12-17 10:29:06 +0100

    medium: disable guttenberg block css - we use classic theme in WP - quicker load for blog pages

--- src/pages/blog/wp-content/themes/mibreit-photo/functions.php
+++ src/pages/blog/wp-content/themes/mibreit-photo/functions.php
@@ -5,6 +5,14 @@ include_once(plugin_dir_path( __FILE__ )."../../../../scripts/affiliate/affiliat
 
 // basic
 
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
 // custom exerpt
 function custom_excerpt_more( $more ) {	
 	return "<a href=\"".get_permalink()."\">[...]</a>";
