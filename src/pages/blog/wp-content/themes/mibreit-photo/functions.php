70bff8067410ee87e63197dfcd1b5c62e8f3a5be
MichaelBreitung <mibreit@gmx.de>
2023-01-22 11:11:41 +0100

    medium: wordpress cleanup - wp_head cleanup - wp_footer cleanup - functions.php structuring

--- src/pages/blog/wp-content/themes/mibreit-photo/functions.php
+++ src/pages/blog/wp-content/themes/mibreit-photo/functions.php
@@ -3,7 +3,9 @@
 <?php
 include_once(plugin_dir_path( __FILE__ )."../../../../scripts/affiliate/affiliate_banner.php");
 
-// basic
+
+
+// ****** CLEANUP ******
 
 //Remove Gutenberg Block Library CSS from loading on the frontend
 function smartwp_remove_wp_block_library_css(){
@@ -12,6 +14,44 @@ function smartwp_remove_wp_block_library_css(){
     wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
 } 
 add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
+// wp_head cleanup
+remove_action( 'wp_head','rsd_link' );
+remove_action( 'wp_head','wlwmanifest_link' );
+remove_action( 'wp_head','locale_stylesheet');
+remove_action( 'wp_head','print_emoji_detection_script', 7);
+remove_action( 'wp_head','wp_print_styles', 8);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action( 'wp_head','wp_print_head_scripts', 9);
+remove_action( 'wp_head','wp_generator' );
+remove_action( 'wp_head','rel_canonical');
+remove_action( 'wp_head','wp_shortlink_wp_head', 10, 0 );
+remove_action( 'wp_head','wp_custom_css_cb', 101 );
+remove_filter( 'wp_robots', 'wp_robots_max_image_preview_large' ); // remove robots
+function remove_api () {
+    remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+}
+add_action( 'after_setup_theme', 'remove_api' );
+
+// wp_footer cleanup
+function wps_deregister_styles() {
+    wp_dequeue_style( 'global-styles' );
+    wp_dequeue_style( 'classic-theme-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'wps_deregister_styles', 100 );
+
+// Remove jQuery Migrate Script from header and Load jQuery from Google API
+function stop_loading_wp_embed() {
+	if (!is_admin()) {
+		wp_deregister_script('wp-embed');		
+	}
+}
+add_action('init', 'stop_loading_wp_embed');
+
+
+
+// ****** CONTENT CUSTOMIZATION ******
 
 // custom exerpt
 function custom_excerpt_more( $more ) {	
@@ -32,6 +72,27 @@ function filter_ptags_on_images($content)
 }
 add_filter('the_content', 'filter_ptags_on_images');
 
+// make images fluid
+function filter_fluid_images ($class){
+    $class .= ' fluid';
+    return $class;
+}
+add_filter('get_image_tag_class','filter_fluid_images');
+
+// make centered images fluid
+function filter_fluid_centered_images($content){        
+    $add_class = str_replace("aligncenter", "aligncenter fluid", $content); 
+    return $add_class;
+}
+add_filter('the_content', 'filter_fluid_centered_images');
+
+// apply small size to image-desc
+function filter_small_image_description($content){        
+    $add_class = str_replace("image-desc", "image-desc small", $content); 
+    return $add_class;
+}
+add_filter('the_content', 'filter_small_image_description');
+
 // remove p tags from around iframes
 function filter_ptags_on_iframes($content)
 {
@@ -48,41 +109,6 @@ function filter_http_to_https($content)
 add_filter('the_content', 'filter_http_to_https');
 {% endif %}
 
-
-// w3c errors
- 
-// uncomment to remove wp-api link in wp header -> Rest API
-//function remove_api () {
-//remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
-//remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
-//}
-//add_action( 'after_setup_theme', 'remove_api' );
-
-// remove emoji scripts
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-remove_action( 'wp_head', 'wp_generator' );
-remove_action( 'wp_head', 'rel_canonical' );
-// remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
-// remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
-// remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
-// remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
-// remove_action( 'wp_head', 'index_rel_link' ); // index link
-// remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
-// remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
-// remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
-
-// Remove jQuery Migrate Script from header and Load jQuery from Google API
-function stop_loading_wp_embed() {
-	if (!is_admin()) {
-		wp_deregister_script('wp-embed');		
-	}
-}
-add_action('init', 'stop_loading_wp_embed');
-
-
-// youtube
-
 // use youtube no cookies embed
 function filter_youtube_no_cookie($content)
 {
@@ -90,38 +116,17 @@ function filter_youtube_no_cookie($content)
 }
 add_filter('the_content', 'filter_youtube_no_cookie');
 
-
-// responsive
-
-// make images fluid
-function filter_fluid_images ($class){
-    $class .= ' fluid';
-    return $class;
-}
-add_filter('get_image_tag_class','filter_fluid_images');
-
-function filter_fluid_centered_images($content){        
-    $add_class = str_replace("aligncenter", "aligncenter fluid", $content); 
-    return $add_class;
-}
-add_filter('the_content', 'filter_fluid_centered_images');
-
-// apply small size to image-desc
-
-function filter_small_image_description($content){        
-    $add_class = str_replace("image-desc", "image-desc small", $content); 
-    return $add_class;
-}
-add_filter('the_content', 'filter_small_image_description');
-
 // make iframe responsive
 function responsive_iframe( $content ) {
     return str_replace(array("<iframe", "</iframe>"), array('<div class="video-container spacing-top-tiny"><iframe', "</iframe></div>"), $content);
 }
 add_filter('the_content', 'responsive_iframe');
 
-// custom functions
 
+
+// ****** CUSTOM FUNCTIONS ******
+
+// retrieve site image
 function get_site_image() {
     global $post, $posts;
     $first_img = '';
@@ -142,23 +147,24 @@ function get_site_image() {
     return $first_img;
 }
 
-// custom replace strings
 
-// replace affiliate shortcodes with affiliate banner
+
+// ****** CUSTOM SHORTCODES ******
+
+// affiliate shortcode setup
 function affiliate_shortcode_func($atts = array())
 {
-$a = shortcode_atts( array(
-'type' => '',
-), $atts );
-return getAffiliateBanner($a['type']);
+    $a = shortcode_atts( array('type' => ''), $atts );
+    return getAffiliateBanner($a['type']);
 }
 add_shortcode( 'affiliate', 'affiliate_shortcode_func' );
 
-// replace before after shortcodes with before after div
+// before after shortcode setup
 function image_before_after_shortcode_func( $atts = array(), $content = null)
 {
 return "<div class=\"imageBeforeAfter\">" . $content . "</div>";
 }
 add_shortcode( 'imageBA', 'image_before_after_shortcode_func' );
+
 ?>
 
