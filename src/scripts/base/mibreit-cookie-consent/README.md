bb0f6fc4d466761d49bb81441b06fc08b9f55091
MichaelBreitung <mibreit@gmx.de>
2023-04-01 16:27:20 +0200

    medium: Improved Cookie Consent for GTM - Opt Out Option now possible in Banner

--- src/scripts/base/mibreit-cookie-consent/README.md
+++ src/scripts/base/mibreit-cookie-consent/README.md
@@ -1,3 +1,35 @@
-Inspired by https://github.com/desarrolladorninja/jquery.cookieMessage.js
+# Simple Cookie Consent Banner
 
-The code was adjusted to suit my specific needs, with changes especially to the css.
+## Purpose
+
+This banner can serve two purposes:
+
+1. Homepage with no Cookies: Inform visitor that no cookies are used and provide a link to privacy policy.
+2. Homepage with Google Analytics: Inform visitor about use of Google Analytics on site and provide him with **opt-out option**. In addition to that, link to privacy policy.
+
+While use case one is simple, number two requires additional setup.
+
+## Usage
+
+Call ``mibreitCookieConsent``, once the website is loaded. This is already setup under **variant-base** under **templates/parts/[en|de]/script-cookie-consent.njk**. It's not compliant to EU Data Protection though. Custom adjustments are required under **variant-custom**.
+
+Here's an example: 
+
+````
+mibreitCookieConsent({'mainMessage': '<p>This website uses cookies from Google Analytics, which provides us with general, anonymized information about the usage of this homepage. We use this information to further improve our offering. You can deactivate the usage of Google Analytics by using the opt-out option. To use this option <a id="ga-opt-out" href="#!">click here</a>. Further information can be found in our <a href="{{page_cookie_consent.en.privacy_url}}#google_analytics">Privacy Policy</a>.</p>', 'acceptButton': 'Got It!', expirationDays: 20, cookieName: 'cookieConsent', gaOptOutId: '#ga-opt-out'});
+````
+
+Note the provision of ``<a id="ga-opt-out" href="#!">click here</a>`` in the paragraph. Providing a link with this id informs the **mibreitCookieConsent** script that Google Analytics is used and an opt out option should be set up.
+
+## Consent Form with GTM
+
+The opt-out option described above only works if Google Analytics is included in the website via Google Tag Manager. It's secure and flexible way of using Google Analytics and There are several tutorials on how to set this up.
+
+If GTM is setup on a page, the following two custom events will be triggered via GTMs **dataLayer**:
+
+1. **GaOptOutEvent** - Visitor clicks on provided opt out link (see above)
+2. **ConsentGivenEvent** - Visitor clicks on accept button
+
+Those events can be used within GTM via **CustomEvent Triggers**.
+
+
--- src/scripts/base/mibreit-cookie-consent/mibreitCookie.js
+++ src/scripts/base/mibreit-cookie-consent/mibreitCookie.js
@@ -1,53 +1,71 @@
 var mibreitCookieConsent = function (config) {
-  var setCookie = function(c_name, value, exdays) {
+  var fireGTMEvent = function (eventName) {
+    if (window['dataLayer']) {
+      window['dataLayer'].push({ event: eventName });
+    }
+  };
+
+  var setConsentCookie = function (cookieName, value, exdays) {
     var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) +  "; samesite=Lax" + (exdays == null ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
-  }
+    var cookieValue = escape(value) + '; samesite=Lax' + (exdays == null ? '' : '; expires=' + exdate.toUTCString());
+    document.cookie = cookieName + '=' + cookieValue;
+  };
 
-  var getCookie = function(c_name) {
-    var i,
-      x,
-      y,
-      ARRcookies = document.cookie.split(";");
-    for (i = 0; i < ARRcookies.length; i++) {
-      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-      y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-      x = x.replace(/^\s+|\s+$/g, "");
-      if (x == c_name) {
-        return unescape(y);
+  var getConsentCookieValue = function (cookieName) {
+    var documentCookies = document.cookie.split(';');
+    for (var i = 0; i < documentCookies.length; i++) {
+      var documentCookieName = documentCookies[i].substr(0, documentCookies[i].indexOf('='));
+      var documentCookieValue = documentCookies[i].substr(documentCookies[i].indexOf('=') + 1);
+      documentCookieName = documentCookieName.replace(/^\s+|\s+$/g, '');
+      if (documentCookieName == cookieName) {
+        return unescape(documentCookieValue);
       }
     }
-  }
+  };
 
-  var cookieMessageGenerate = function() {
+  var giveConsent = function () {
+    setConsentCookie(config.cookieName, true, config.expirationDays);
+    fireGTMEvent('ConsentGivenEvent');
+  };
+
+  var setupGaOptOut = function () {
+    document.querySelector('#cookie-msg ' + config.gaOptOutId).addEventListener('click', function (event) {
+      event.preventDefault();
+      fireGTMEvent('GaOptOutEvent');
+    });
+  };
+
+  var setupCookieConsent = function () {
+    document.querySelector('#cookie-msg .mibreit-cookie-accept').addEventListener('click', function (event) {
+      event.preventDefault();
+      giveConsent();
+      cookieConsentBar.remove();
+      cookieConsentDarkening.remove();
+    });
+  };
+
+  var cookieValue = getConsentCookieValue(config.cookieName);
+  if (cookieValue !== 'true') {
     var html =
       '<div>' +
       config.mainMessage +
       '</div><div class="mibreit-cookie-accept"><a href="#">' +
       config.acceptButton +
-      "</a></div>";
-    var cookieConsentBar = document.createElement("div");
-    cookieConsentBar.setAttribute("id", "cookie-msg");
+      '</a></div>';
+    var cookieConsentBar = document.createElement('div');
+    cookieConsentBar.setAttribute('id', 'cookie-msg');
     cookieConsentBar.innerHTML = html;
-    var cookieConsentDarkening = document.createElement("div");
-    cookieConsentDarkening.setAttribute("id", "cookie-msg__background");
-    var body = document.querySelector("body");
+    var cookieConsentDarkening = document.createElement('div');
+    cookieConsentDarkening.setAttribute('id', 'cookie-msg__background');
+    var body = document.querySelector('body');
     body.append(cookieConsentDarkening);
     body.append(cookieConsentBar);
-    document
-      .querySelector("#cookie-msg .mibreit-cookie-accept")
-      .addEventListener("click", function (event) {       
-        event.preventDefault();
-        setCookie(config.cookieName, true, config.expirationDays);
-        cookieConsentBar.remove();
-        cookieConsentDarkening.remove();
-      });
+
+    if (typeof config.gaOptOutId === 'string') {
+      setupGaOptOut();
+    }
+
+    setupCookieConsent();
   }
-  
-  var cookie = getCookie(config.cookieName);
-  if (cookie !== "true") {
-    cookieMessageGenerate();
-  }  
 };
