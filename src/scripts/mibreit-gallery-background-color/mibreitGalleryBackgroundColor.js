4104b1cfd6141b0d7142f255f32215b4adbd0b80
MichaelBreitung <mibreit@gmx.de>
2023-06-10 13:48:36 +0200

    minor: fixed potential undefined variable - case with no images in blog

--- src/scripts/mibreit-gallery-background-color/mibreitGalleryBackgroundColor.js
+++ src/scripts/mibreit-gallery-background-color/mibreitGalleryBackgroundColor.js
@@ -1,31 +1,28 @@
 var mibreitGalleryBackgroundColor = function (gallery) {
   var fullscreen = gallery.getFullscreen();
-  if (typeof FastAverageColor !== "undefined")
-  {
-    var viewer = gallery.getViewer();
-    var fastAverageColor = new FastAverageColor();
-    var fastAverageColorOptions = {algorithm: 'sqrt'};
+  var viewer = gallery.getViewer();
+  if (fullscreen && viewer) {
+    if (typeof FastAverageColor !== 'undefined') {
+      var fastAverageColor = new FastAverageColor();
+      var fastAverageColorOptions = { algorithm: 'sqrt' };
 
-    var updateBackgroundColor = function(imageElement)
-    {
-      try{
-        var color = fastAverageColor.getColor(imageElement, fastAverageColorOptions);
-        fullscreen.setBackgroundColor(color.rgba);
-      }
-      catch (e)
-      {
-        console.warn("FastAverageColor Exception: "+e);
-      }
+      var updateBackgroundColor = function (imageElement) {
+        try {
+          var color = fastAverageColor.getColor(imageElement, fastAverageColorOptions);
+          fullscreen.setBackgroundColor(color.rgba);
+        } catch (e) {
+          console.warn('FastAverageColor Exception: ' + e);
+        }
+      };
+
+      updateBackgroundColor(viewer.getImageElement(viewer.getImageIndex()));
+
+      viewer.addImageChangedCallback(function (index, _imageInfo) {
+        updateBackgroundColor(viewer.getImageElement(index));
+      });
+    } else {
+      console.warn('no FastAverageColor installed');
+      fullscreen.setBackgroundColor("#000000");
     }
-
-    updateBackgroundColor(viewer.getImageElement(viewer.getImageIndex()));
-  
-    viewer.addImageChangedCallback(function(index, _imageInfo) {
-      updateBackgroundColor(viewer.getImageElement(index));
-    });
   }
-  else{
-    console.warn("no FastAverageColor installed");
-    fullscreen.setBackgroundColor("#000000");
-  }
-} 
+};
