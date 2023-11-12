23a213cd3544ea5a6fe2c6c51210361fb4253366
MichaelBreitung <mibreit@gmx.de>
2023-11-12 14:59:40 +0100

    minor: optional parameter for hamburgerNavbar

--- src/scripts/base/hamburger-navbar/hamburgerNavbar.js
+++ src/scripts/base/hamburger-navbar/hamburgerNavbar.js
@@ -1,6 +1,11 @@
-var hamburgerNavbar = function () {
-  var navigationContainer = document.querySelector('.navigation__container');
-  var menuBtn = document.querySelector('.navigation__container .menu-btn');  
+var hamburgerNavbar = function (containerClassOverride) {
+  var containerClass = ".navigation__container";
+  if (containerClassOverride)
+  {
+    containerClass = containerClassOverride;
+  }
+  var navigationContainer = document.querySelector(containerClass);
+  var menuBtn = document.querySelector(containerClass+' .menu-btn');  
   var isOpen = false;  
 
   var update = function() {
