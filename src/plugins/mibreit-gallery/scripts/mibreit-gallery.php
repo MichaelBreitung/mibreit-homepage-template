2af22a21d44d08388af1a5a9a593609e25f41adf
MichaelBreitung <mibreit@gmx.de>
2024-03-31 10:36:14 +0200

    medium: fixed missing variables

--- src/plugins/mibreit-gallery/scripts/mibreit-gallery.php
+++ src/plugins/mibreit-gallery/scripts/mibreit-gallery.php
@@ -3,11 +3,14 @@
 class MibreitGalleryImage
 {
   public $caption;
-  public $altEn;   
-  public $altDe;        
+  public $altEn;
+  public $altDe;
   public $imageUrl;
   public $thumbUrl;
   public $prints;
+  public $woocommerce;
+  public $limited;
+  public $size;
 
   function __construct()
   {
@@ -22,54 +25,45 @@ class MibreitGalleryImage
     $this->size = "large";
   }
 
-  function initFromXml($imageXml, $path, $thumbPath, $relativePath) {
+  function initFromXml($imageXml, $path, $thumbPath, $relativePath)
+  {
     $tmp = $imageXml->filename;
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->imageUrl = $relativePath . $path . (string) $tmp;
       $this->thumbUrl = $relativePath . $thumbPath . (string) $tmp;
 
       $tmp = $imageXml->caption;
-      if (!empty($tmp))
-      {
+      if (!empty($tmp)) {
         $this->caption = (string) $tmp;
         $this->altEn = (string) $tmp;
         $this->altDe = (string) $tmp;
       }
-      $tmp = $imageXml->alt;    
-      if (!empty($tmp))
-      {    
+      $tmp = $imageXml->alt;
+      if (!empty($tmp)) {
         $this->altEn = (string) $tmp;
       }
       $tmp = $imageXml->altDe;
-      if (!empty($tmp))
-      {
+      if (!empty($tmp)) {
         $this->altDe = (string)  $tmp;
-      }   
+      }
       $prints = $imageXml->prints;
-      if (!empty($prints))
-      {
+      if (!empty($prints)) {
         $this->prints = true;
         $tmp = $prints["woocommerce"];
-        if (!empty($tmp))
-        {
+        if (!empty($tmp)) {
           $this->woocommerce = (string) $tmp;
         }
         $tmp = $prints["limited"];
-        if (!empty($tmp))
-        {
-          $this->limited = (boolean) $tmp;
+        if (!empty($tmp)) {
+          $this->limited = (bool) $tmp;
         }
         $tmp = $prints["size"];
-        if (!empty($tmp))
-        {
+        if (!empty($tmp)) {
           $this->size = (string) $tmp;
         }
       }
       return true;
-    } 
-    else 
-    {
+    } else {
       return false;
     }
   }
@@ -90,60 +84,52 @@ class MibreitGalleryInfo
   public $content_title;
   public $content;
 
-  function __construct($de) {
+  function __construct($de)
+  {
     $this->header_h1 = "{{page_name}}";
     $this->title = "{{page_name}}";
-    $this->content_title = "";    
+    $this->content_title = "";
     $this->content_category = "";
     $this->content = "";
-    if ($de)
-    {
+    if ($de) {
       $this->keywords = "Fotografie, Foto Galerie";
-      $this->description = "Galerie mit Reise und Landschaftsfotos";    
-    }
-    else{      
+      $this->description = "Galerie mit Reise und Landschaftsfotos";
+    } else {
       $this->keywords = "Photo Gallery, Photography";
-      $this->description = "Photo gallery with beautiful landscape and cityscape photos";    
-    }   
+      $this->description = "Photo gallery with beautiful landscape and cityscape photos";
+    }
   }
 
   public function initFromXml($infoXml)
   {
     $tmp = $infoXml->header_h1;
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->header_h1 = (string) $tmp;
     }
     $tmp = $infoXml->content_category;
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->content_category = (string) $tmp;
     }
     $tmp = $infoXml->content_title;
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->content_title = (string) $tmp;
     }
     $tmp = $infoXml->content;
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->content = (string) $tmp;
     }
     $tmp = $infoXml["keywords"];
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->keywords = (string) $tmp;
     }
     $tmp = $infoXml["description"];
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->description = (string) $tmp;
     }
     $tmp = $infoXml["title"];
-    if (!empty($tmp))
-    {
+    if (!empty($tmp)) {
       $this->title = (string) $tmp;
-    }    
+    }
   }
 }
 
@@ -158,57 +144,48 @@ class MibreitGalleryDataParser
 
   function __construct($file)
   {
-    $this->infoEn = new MibreitGalleryInfo(false);  
+    $this->infoEn = new MibreitGalleryInfo(false);
     $this->infoDe = new MibreitGalleryInfo(true);
 
     // get relative path part of file to use for image paths too
     $relativePath = "";
     $relativePathEndPos = strrpos($file, "/");
-    if ($relativePathEndPos != false)
-    {
-      $relativePath = substr($file, 0, $relativePathEndPos+1);
+    if ($relativePathEndPos != false) {
+      $relativePath = substr($file, 0, $relativePathEndPos + 1);
     }
 
-    if (file_exists($file))
-    {
-      $gallery = simplexml_load_file($file);    
-   
+    if (file_exists($file)) {
+      $gallery = simplexml_load_file($file);
+
       $info = $gallery->infoEn;
-      if (!empty($info))
-      {
+      if (!empty($info)) {
         $this->infoEn->initFromXml($info);
       }
-      
+
       $info = $gallery->infoDe;
-      if (!empty($info))
-      {
+      if (!empty($info)) {
         $this->infoDe->initFromXml($info);
-      }  
-       
+      }
+
       $images = $gallery->images;
       $imagePath = "";
-      if (!empty($gallery[self::IMAGE_PATH_TAG]))
-      {
+      if (!empty($gallery[self::IMAGE_PATH_TAG])) {
         $imagePath = (string) $gallery[self::IMAGE_PATH_TAG];
       }
       $thumbPath = "";
-      if (!empty($gallery[self::THUMB_PATH_TAG]))
-      {
+      if (!empty($gallery[self::THUMB_PATH_TAG])) {
         $thumbPath = (string) $gallery[self::THUMB_PATH_TAG];
       }
-  
-      if (!empty($images))
-      {
-        foreach ($images->children() as $imageXml)
-        {
+
+      if (!empty($images)) {
+        foreach ($images->children() as $imageXml) {
           $image = new MibreitGalleryImage();
-          if ($image->initFromXml($imageXml, $imagePath, $thumbPath, $relativePath))
-          {
+          if ($image->initFromXml($imageXml, $imagePath, $thumbPath, $relativePath)) {
             array_push($this->images, $image);
           }
-        }  
-      } 
-    }   
+        }
+      }
+    }
   }
 
   public function getNumberImages()
@@ -226,27 +203,22 @@ class MibreitGalleryDataParser
     $randomImages = [];
     $nrImages = $this->getNumberImages();
     $images = $this->images;
-    for ($i = 0; $i < $number and $nrImages > 0; $i++)
-    {
-        $r = rand(0, $nrImages - 1);
-        array_push($randomImages, $images[$r]);
-        array_splice($images, $r, 1);
-        $nrImages--;
+    for ($i = 0; $i < $number and $nrImages > 0; $i++) {
+      $r = rand(0, $nrImages - 1);
+      array_push($randomImages, $images[$r]);
+      array_splice($images, $r, 1);
+      $nrImages--;
     }
     return $randomImages;
-  } 
+  }
 
   public function getImage($id)
   {
-    if (array_key_exists($id, $this->images))
-    {
+    if (array_key_exists($id, $this->images)) {
       return $this->images[$id];
-    }
-    elseif (count($this->images)) {
+    } elseif (count($this->images)) {
       return $this->images[0];
-    }
-else 
-    {
+    } else {
       return null;
     }
   }
@@ -254,15 +226,14 @@ else
   public function getPrintInfos()
   {
     $printInfos = array();
-    foreach ($this->images as $image)
-    {             
-      $printInfos[$image->imageUrl] = $image->getPrintInfo();      
-    } 
+    foreach ($this->images as $image) {
+      $printInfos[$image->imageUrl] = $image->getPrintInfo();
+    }
     return $printInfos;
   }
 
   public function getInfoEn()
-  {    
+  {
     return $this->infoEn;
   }
 
@@ -271,4 +242,3 @@ else
     return $this->infoDe;
   }
 }
-?>
